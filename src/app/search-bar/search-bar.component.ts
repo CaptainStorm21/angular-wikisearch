879ee8f3714982258c3ddb130ed2fb9fb87bd3ba
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-03 23:28:17 -0700

    created onInput function $event.target.value

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  term = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // onInput(value: string) {
+  //   this.term = value;
+  // }
+  // html (input)="onInput($event.target.value)"
 
 }
