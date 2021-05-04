7c5981cca2a3360493208449300d0de5734518f0
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-05-03 23:18:18 -0700

    updated search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,7 @@ import { PageListComponent } from './page-list/page-list.component';
     PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
